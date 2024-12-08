957c9dbdd62c7915dce52a4b28778b2ff1e7c381
keerthana2624 <keerthanamummy2002@gmail.com>
2024-12-08 14:54:57 +0530

    imported the menuRouted and added meniroutes in the index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const connectToDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const restaurantRoute = require('./routes/restaurantRoute');
+const menuRoutes = require('./routes/menuRoutes'); // Import menuRoutes
 const app = express()
 
 connectToDB();
@@ -12,6 +13,7 @@ app.use(express.json());
 
 app.use('/api/users', userRoutes);
 app.use('/api/restaurant', restaurantRoute);
+app.use('/api/menus', menuRoutes); // Add menuRoutes
 
 app.get('/',(req,res) =>{
     res.status(200).send({message:'Sucess'})
