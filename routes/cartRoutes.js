f892912c580c5ce13e00488dbc58c22a62d68f9a
keerthana2624 <keerthanamummy2002@gmail.com>
2024-12-08 16:30:44 +0530

    given the router for update a quantity of a menu in the cart

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -8,4 +8,6 @@ router.post('/add', cartController.addToCart);
 
 // Remove a menu item from the cart
 router.delete('/remove', cartController.removeFromCart);
+// Update the quantity of a menu item in the cart
+router.put('/update', cartController.updateCartItemQuantity);
 module.exports = router;
