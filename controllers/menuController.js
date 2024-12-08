c10b81cf286d2b3702ce11527c6c41085c1d1b84
keerthana2624 <keerthanamummy2002@gmail.com>
2024-12-08 14:50:01 +0530

    implimented the logic for add a new menu item

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -3,3 +3,32 @@ const Restaurant = require('../models/restaurantModel');
 
 
 
+// Add a new menu item
+exports.addMenuItem = async (req, res) => {
+    const { name, description, price, imageUrl, restaurantId } = req.body;
+
+    try {
+        const restaurant = await Restaurant.findById(restaurantId);
+        if (!restaurant) {
+            return res.status(404).json({ message: 'Restaurant not found' });
+        }
+
+        const newMenuItem = new MenuItem({
+            name,
+            description,
+            price,
+            imageUrl,
+            restaurant: restaurantId,
+        });
+
+        await newMenuItem.save();
+
+        // Add menu item to the restaurant's menu
+        restaurant.menuItems.push(newMenuItem._id);
+        await restaurant.save();
+
+        res.status(201).json({ message: 'Menu item added successfully', menuItem: newMenuItem });
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to add menu item', error });
+    }
+};
