a67944b401944863a4059c73f7f6c0b8ea8e7116
keerthana2624 <keerthanamummy2002@gmail.com>
2024-12-08 16:16:09 +0530

    given the route to add a menu item to the cart

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,2 +1,7 @@
 const express = require('express');
 const cartController = require('../controllers/cartController');
+const router = express.Router();
+
+
+// Add a menu item to the cart
+router.post('/add', cartController.addToCart);
