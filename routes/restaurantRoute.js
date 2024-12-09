58b96c560dc7e44099c86a670e0ab25439e37091
keerthana2624 <keerthanamummy2002@gmail.com>
2024-12-09 16:57:19 +0530

    created a checkmodel component

--- routes/restaurantRoute.js
+++ routes/restaurantRoute.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const restaurantController = require('../controllers/restaurantController')
+
+
 const router = express.Router();
 
 // add a new restaurant
 router.post('/',restaurantController.addNewRestaurant);
-router.get('/:id', restaurantController.getRestaurantById)
-router.get('/',restaurantController.getAllRestaurants);
+router.get('/:id', restaurantController.getRestaurantById);
+router.get('/', restaurantController.getAllRestaurants);
 router.put('/:id',restaurantController.updateRestaurant);
 router.delete('/:id',restaurantController.deleteRestaurant);
 module.exports = router;
